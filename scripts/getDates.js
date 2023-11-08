55081b7a973f66d1c94bf16f0324f1e57b2d2e16
bux <bmakhanya424@gmail.com>
2023-11-08 17:39:57 +0200

    almost

---
+++ scripts/getDates.js
@@ -0,0 +1,8 @@
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+
+document.querySelector('.year').innerHTML = currentYear;
+
+
+var latesDate = document.lastModified;
+document.querySelector('#lastModified').innerHTML = latesDate;
