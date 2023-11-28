5e949255414f9a455fce779c4a66aaebcba612fb
bux <bmakhanya424@gmail.com>
2023-11-28 10:03:35 +0200

    hom

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -33,4 +33,19 @@ modeButton.addEventListener("click", () => {
 	}
 });
 
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
+
 
