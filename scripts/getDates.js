ad55123eb0e26d4bd0f74160fdf4ed19d872d302
bux <bmakhanya424@gmail.com>
2024-01-06 20:08:34 +0200

    e

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -11,10 +11,10 @@ const mainnav = document.querySelector('nav');
 const hambutton = document.querySelector('.menu');
 
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-hambutton.addEventListener('click', () => {
+`hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
-});
+});`
 
 const modeButton = document.querySelector("#mode");
 const header = document.querySelector('header');
@@ -53,14 +53,14 @@ const key = document.querySelector("secure");
 
 const message = document.querySelector("formMsg");
 
-key.addEventListener("same", checkSame);
+//key.addEventListener("same", checkSame);
 
 
 const kp1 = document.querySelector("#password");
 const kp2 = document.querySelector("#password2");
 const messages = document.querySelector("#message");
 
-kp2.addEventListener("focusout", checkSame);
+//kp2.addEventListener("focusout", checkSame);
 function checkSame() {
 	if (kp1.value !== kp2.value) {
 		messages.textContent = "Passwords DO NOT MATCH!";
@@ -81,15 +81,15 @@ function checkSame() {
 const rangeValue = document.getElementById("calibration");
 const range = document.getElementById("values");
 
-range.addEventListener('change', displayRatingValue);
-range.addEventListener('input', displayRatingValue);
+//range.addEventListener('change', displayRatingValue);
+//range.addEventListener('input', displayRatingValue);
 
 function displayRatingValue() {
     rangevalue.innerHTML = range.value;
 }
 
-range.addEventListener('change', displaySelection);
-range.addEventListener('input', displaySelection);
+//range.addEventListener('change', displaySelection);
+//range.addEventListener('input', displaySelection);
 
 function displaySelection() {
 	rangeValue.innerHTML = range.value;
@@ -104,27 +104,29 @@ const weatherIcon = document.querySelector('#wicon');
 const climate = document.querySelector('#climate');
 
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?-26.397769652501967, 31.173061312871386&units=imperial&appid=5443c1edf2b4aa0ca3be31167228081e';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=-26.40&lon=31.17&units=metric&appid=4d7e5824d50954c80fb74fd719d28184`;
+const focs = `api.openweathermap.org/data/2.5/forecast?lat=-26.40&lon=31.17&appid=4d7e5824d50954c80fb74fd719d28184`
 
 async function fetchWeatherApi() {
-  try {
-    const response = await fetch(url);
-    if (response.ok) {
-      const data = await response.json();
-      displayResults(data);
-      console.log(data);
-    } else {
-        throw Error(await response.text());
-    }
-  } catch (error) {
-      console.log(error);
+	try {
+	  const response = await fetch(url);
+	  if (response.ok) {
+		const data = await response.json();
+		displayResults(data);
+		console.log(data);
+	  } else {
+		  throw Error(await response.text());
+	  }
+	} catch (error) {
+		console.log(error);
+	}
   }
-}
 
 const displayResults = (weather) => {
+	currentTemp.textContent = `${weather.main.temp}`
     weatherIcon.setAttribute('src', `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
-    weatherIcon.setAttribute('alt', `${weather.weather[0].main}`);
-    climate.textContent = `${weather.weather[0].description}`
+	climate.textContent = `${weather.weather[0].description}`;
+   
 };
 
 fetchWeatherApi();
