71b99582421047deac2436184262fab11a2e0825
Banele Makhanya <bmakhanya424@gmail.com>
2024-10-14 09:01:30 +0200

    bom

---
+++ week02/scripts/bom.js
@@ -0,0 +1,20 @@
+const list = document.querySelector('#list');
+const button = document.querySelector('button');
+const input = document.querySelector('#favchap');
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        const li = document.createElement('li');
+        const del = document.createElement('button');
+        li.textContent = input.value;
+        del.textContent = '❌';
+        li.append(del);
+        list.append(li);
+        input.focus();
+        del.addEventListener('click', () => {
+            list.removeChild(li);
+            input.focus();
+            input.value = '';
+        })
+    }
+})
