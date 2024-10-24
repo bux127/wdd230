92064c2e17aaaf1f2b53a8f6495bdbec00c690aa
Banele Makhanya <bmakhanya424@gmail.com>
2024-10-24 19:33:03 +0200

    new

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -14,3 +14,34 @@ document.querySelectorAll('.nav_link').forEach(n => n.addEventListener('click', 
     nav.classList.remove('active')
 }))
 
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+const key = document.querySelector("#keyphrase");
+const key1 = document.querySelector("#keyphrase2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame());
+
+// This should be refactored.
+function checkSame() {
+	if (key.value !== key1.value) {
+		message.textContent = "❗Key Phrases DO NOT MATCH!";
+		message.style.visibility = "show";
+		key1.style.backgroundColor = "#fff0f3";
+		key1.value = "";
+		key1.focus();
+	} else {
+		message.style.display = "none";
+		key1.style.backgroundColor = "#fff";
+		key1.style.color = "#000";
+	}
+}
