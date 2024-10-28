934fb006b9eb6c25f18b2bf24c03464fab775839
Banele Makhanya <bmakhanya424@gmail.com>
2024-10-29 02:01:57 +0800

    5

---
+++ week05/scripts/prophets.js
@@ -0,0 +1,25 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards')
+
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        console.table(data);
+        displayProphets(data.prophets);
+    }
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('div');
+        card.innerHTML = `<h2>${prophet.name} ${prophet.lastname}</h2>
+                          <p>Date of birth: ${prophet.birthdate}</p>
+                          <p>Place of birth: ${prophet.birthplace}</p>
+                          <img src="${prophet.imageurl}" alt="${prophet.name} ${prophet.lastname}">`;
+        cards.append(card);
+    });
+}
+
+getProphetData(url);
